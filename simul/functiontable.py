b21bfdb673b73af7b485cbb5ad5cb625217512e8
mechu1205 <mechu1205@gmail.com>
2021-06-07 05:09:21 +0900

    updated functiontable.py and simulator.py

--- simul/functiontable.py
+++ simul/functiontable.py
@@ -1,3 +1,5 @@
+import copy
+
 class FunctionTableEntry:
     def __init__(self, id, return_type, line_number, parameters, statements):
         self.id = id
@@ -5,7 +7,46 @@ class FunctionTableEntry:
         self.line_number = line_number
         self.parameters = parameters
         self.statements = statements
+        self.context = None # Needs to be assigned by the caller
 
 class FunctionTable:
     def __init__(self):
         self.table = list()
+    
+    def define(self, function, context):
+        fte = FunctionTableEntry(
+            function["id"],
+            function["return_type"],
+            function["line_number"],
+            function["parameters"],
+            function["statements"],
+        )
+        # copy context at definition; recursion is not possible
+        fte.context = copy.deepcopy(context)
+        
+        self.table.append(fte)
+    
+    def call(self, function_id, arguments):
+        # function_id (str): name of function
+        # arguments (list): Constants (not Expressions!)
+        
+        if function_id not in self.table:
+            # Undefined Function
+            raise RuntimeError(None)
+        
+        else:
+            # Parameter number and type matching
+            func = self.table[function_id]
+            if len(arguments) != len(func.parameters):
+                raise RuntimeError(None)
+            
+            else:
+                context = copy.deepcopy(func.context)
+                for (argument, parameter) in zip(arguments, func.parameters):
+                    if argument["type"] != parameter["type"]:
+                        # might be parameter[1]; depends on AST implementation
+                        raise RuntimeError(None)
+                
+                # function context is returned "as is"
+                # parameter values should be assigned by caller
+                return func.context, func.return_type, func.line_number, func.parameters, func.statements
--- simul/simulator.py
+++ simul/simulator.py
@@ -10,8 +10,86 @@ class State:
         self.ftable = FunctionTable()
         self.heap = None
 
-if __name__ == "__main__":
-    while(True):
-        command = input(">> ")
-        print(command)
+def parse_cli(cli):
+    tokens = cli.split()
+    if len(tokens) == 0:
+        return False, None, None
     
+    cmd = tokens[0].lower()
+    if (cmd == "help"):
+        print(
+            "next [LINES]\n"
+            "print VARIABLE\n"
+            "trace VARIABLE\n"
+            "mem"
+            "exit"
+        )
+        return False, None, None
+    
+    elif (cmd == "next"):
+        msg = "Incorrect command usage : try \'next [lines]\'"
+        lines = 0
+        if len(tokens) == 1:
+            lines = 1
+        elif len(tokens) > 2:
+            None
+        elif not tokens[1].isdigit():
+            None
+        else:
+            lines = int(tokens[1])
+        
+        if lines <= 0:
+            print(msg)
+            return False, None, None
+        else:
+            return True, "next", lines
+        
+    elif (cmd == "print" or cmd == "trace"):
+        msg1 = "Incorrect command usage : try \'{} [variable]\'".format(cmd)
+        msg2 = "Invalid typing of the variable name"
+        id = None
+        if len(tokens) != 2:
+            print(msg1)
+            None
+        elif not tokens[1].isidentifier():
+            print(msg2)
+            None
+        else:
+            id = tokens[1]
+        
+        return bool(id), cmd, id
+    
+    elif (cmd == "mem"):
+        msg = "Incorrect command usage : try \'mem\'"
+        valid = (len(tokens) == 1)
+        if not valid:
+            print(msg)
+        
+        return valid, cmd, None
+    
+    elif (cmd == "exit"):
+        msg = "Incorrect command usage : try \'exit\'"
+        valid = (len(tokens) == 1)
+        if not valid:
+            print(msg)
+        
+        return valid, cmd, None
+    
+    else:
+        print("Incorrect command usage: try \'help\'")
+        return False, None, None
+
+
+if __name__ == "__main__":
+    do_next_loop = True
+    while(do_next_loop):
+        cli = input(">> ")
+        valid, cmd, arg = parse_cli(cli)
+        
+        if not valid:
+            None
+        else:
+            if (cmd == "exit"):
+                do_next_loop = False
+            else:
+                print(cmd, arg)
