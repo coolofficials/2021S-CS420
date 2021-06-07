55d437e65ab4a2ea44d17503efc1b5a7dc8ac0e6
coolofficials <coolofficials@gmail.com>
2021-06-07 12:53:27 +0000

    add tag, debug parse

--- parser/parser.py
+++ parser/parser.py
@@ -40,7 +40,9 @@ class AST:
             code_line = self.codes.pop(0)
             if code_line[-1] == ";":
                 body = [code_line]
-                self.roots.append(Statement().parse(body))
+                statement = Statement()
+                statement.parse(body)
+                self.roots.append(statement)
 
             # TODO: Need to handle better for nested for, if, function, etc. (Count { and }).
             elif code_line[-1] == "{":
@@ -63,67 +65,81 @@ class Statement:
     def __init__(self):
         # Child node by parsing.
         self.child
+        self.tag = "Statement"
 
     # Perform appropriate parsing for the statement.
     # body: A list of lines of code.
     # TODO: parse, especially parsing statements... handle nested parenthesis.
     def parse(self, body):
         key = getFirstWord(body[0])
+
+        # TODO: statements.
         if key == "if":
             # get conditions and body.
             # body[0] = 'if (conditon) {'
             # body[1~-2] = self.child.body, statements.
             # body[-1] = '}'
-            self.child = If().parse(condition, statements)
+            self.child = If()
+            self.child.parse(condition, statements)
+
+        # TODO: statements.
         elif key == "for":
             # body[0] = 'for (initializer: assignment; condition: expr; step: expr) {'
             # body[1~-2] = self.child.body, statements.
             # body[-1] = '}'
-            self.child = For().parse(initializer, condition, step, statements)
+            self.child = For()
+            self.child.parse(initializer, condition, step, statements)
 
         # Done
         elif key == "return":
             # body[0] = 'return expr;'
             expr = body[0].split(" ", 1)[1][:-1]
-            self.child = Return().parse(expr)
+            self.child = Return()
+            self.child.parse(expr)
 
         # TODO: get token
         elif key == "printf":
             # body[0] = 'printf(token);'
-            self.child = Printf().parse(token)
+            self.child = Printf()
+            self.child.parse(token)
 
         # Done
         elif key == "Free":
             # body[0] = 'free (variable);'
             variable = body[0].split(" ", 1)[1][1:-2]
-            self.child = Free().parse(variable)
+            self.child = Free()
+            self.child.parse(variable)
 
         # TODO: get params & statements.
         elif key in {"int", "float"} and body[0][-1] == "{":
             # body[0] = 'int/float identifier (parameters) {'
             signature = key
             identifier = getSecondWord(body[0])
-            self.child = Function().parse(signature, identifier, parameters, statements)
+            self.child = Function()
+            self.child.parse(signature, identifier, parameters, statements)
 
         # Done
         elif key in {"int", "float"}:
             # body[0] = 'int/float identifier;'
             signature = key
             identifier = getSecondWord(body[0])
-            self.child = Declaration().parse(signature, identifier)
+            self.child = Declaration()
+            self.child.parse(signature, identifier)
 
         # Done
         elif getSecondWord(body[0]) == "=":
             # body[0] = 'identifier = expr;'
             identifier = body[0].split(" ")[0]
             expr = body[0].split(" ", 2)[2][:-1]
-            self.child = Assignment().parse(identifier, expr)
+            self.child = Assignment()
+            self.child.parse(identifier, expr)
 
         # Done
         else:
             # body[0] = 'expr;'
             expr = body[0][:-1]
-            self.child = Expression().parse(expr)
+            self.child = Expression()
+            self.child.parse(expr)
 
 
 # Done
@@ -131,9 +147,14 @@ class If:
     def __init__(self):
         self.condition
         self.statements = []
+        self.tag = "If"
 
     def parse(self, condition, statements):
-        self.condition = Expression().parse(condition)
+        self.condition = Statement()
+        self.condition.parse([condition])
+        if self.condition.child.tag != "Expression":
+            # TODO: explicit error handler.
+            print("syntax error")
         for statement in statements:
             self.statements.append(Statement().parse(statement))
 
@@ -145,28 +166,37 @@ class For:
         self.condition
         self.step
         self.statements = []
+        self.tag = "For"
 
     def parse(self, initializer, condition, step, statements):
-        self.initializer = Assignment().parse(initializer)
-        self.condition = Expression().parse(condition)
-        self.step = Expression().parse(step)
+        self.initializer = Assignment()
+        self.initializer.parse(initializer)
+        self.condition = Expression()
+        self.condition.parse(condition)
+        self.step = Expression()
+        self.step.parse(step)
         for statement in statements:
-            self.statements.append(Statement().parse(statement))
+            stmt = Statement()
+            stmt.parse(statement)
+            self.statements.append(stmt)
 
 
 # Done
 class Return:
     def __init__(self):
         self.expr
+        self.tag = "Return"
 
     def parse(self, expr):
-        self.expr = Expression().parse(expr)
+        self.expr = Expression()
+        self.expr.parse(expr)
 
 
 # TODO: Specify Token
 class Printf:
     def __init__(self):
         self.token
+        self.tag = "Printf"
 
     def parse(self, token):
         self.token = token
@@ -176,9 +206,11 @@ class Printf:
 class Free:
     def __init__(self):
         self.variable
+        self.tag = "Free"
 
     def parse(self, variable):
-        self.variable = Variable().parse(variable)
+        self.variable = Variable()
+        self.variable.parse(variable)
 
 
 # Done
@@ -186,6 +218,7 @@ class Declaration:
     def __init__(self):
         self.signature
         self.identifier
+        self.tag = "Declaration"
 
     def parse(self, signature, identifier):
         self.signature = signature
@@ -199,14 +232,19 @@ class Function:
         self.identifier
         self.parameters = []
         self.statements = []
+        self.tag = "Function"
 
     def parse(self, signature, identifier, parameters, statements):
         self.signature = signature
         self.identifier = identifier
         for param in parameters:
-            self.parameters.append(Parameter().parse(param))
+            parameter = Parameter()
+            parameter.parse(param)
+            self.parameters.append(parameter)
         for statement in statements:
-            self.statements.append(Statement().parse(statement))
+            stmt = Statement()
+            stmt.parse(statement)
+            self.statements.append(stmt)
 
 
 # Done
@@ -214,6 +252,7 @@ class Assignment:
     def __init__(self):
         self.identifier
         self.expr
+        self.tag = "Assignment"
 
     def parse(self, identifier, expr):
         self.identifier = identifier
@@ -224,6 +263,7 @@ class Assignment:
 class Expression:
     def __init__(self):
         self.type
+        self.tag = "Expression"
 
     def parse(self, expr):
         pass
@@ -233,6 +273,7 @@ class Expression:
 class Calculation:
     def __init__(self):
         self.type
+        self.tag = "Calculation"
 
     def parse(self):
         pass
@@ -241,6 +282,7 @@ class Calculation:
 class BinaryOp:
     def __init__(self, statement):
         self.type
+        self.tag = "BinaryOp"
 
     def parse(self):
         pass
@@ -249,6 +291,7 @@ class BinaryOp:
 class UnaryOp:
     def __init__(self, statement):
         self.type
+        self.tag = "UnaryOp"
 
     def parse(self):
         pass
@@ -257,6 +300,7 @@ class UnaryOp:
 class Factor:
     def __init__(self, statement):
         self.type
+        self.tag = "Factor"
 
     def parse(self):
         pass
@@ -264,7 +308,7 @@ class Factor:
 
 class Parameter:
     def __init__(self):
-        pass
+        self.tag = "Parameter"
 
     def parse(self):
         pass
@@ -273,6 +317,7 @@ class Parameter:
 class Variable:
     def __init__(self, statement):
         self.type
+        self.tag = "Variable"
 
     def parse(self):
         pass
@@ -281,6 +326,7 @@ class Variable:
 class Constant:
     def __init__(self, statement):
         self.type
+        self.tag = "Constant"
 
     def parse(self):
         pass
@@ -289,6 +335,7 @@ class Constant:
 class FunctionCall:
     def __init__(self, statement):
         self.type
+        self.tag = "FunctionCall"
 
     def parse(self):
         pass
