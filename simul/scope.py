71d59e7aab8bee6559214646e30995d2bb32fbdf
mechu1205 <mechu1205@gmail.com>
2021-06-08 17:17:50 +0900

    update on state.py

--- simul/scope.py
+++ simul/scope.py
@@ -6,4 +6,9 @@ class Scope:
     def __init__(self):
         self.history = History()
         self.ftable = FunctionTable()
-
+    
+    def deepcopy(self, delta_layer = 0):
+        scope = Scope()
+        scope.history = self.history.deepcopy(delta_layer)
+        scope.ftable = copy.deepcopy(self.ftable)
+        return scope
--- simul/state.py
+++ simul/state.py
@@ -14,16 +14,48 @@ class State:
         self.ip = 0
         self.goback = deque()
     
-    def next(self):
-        if self.ip >= len(self.statements):
-            if(not self.goback):
+    def do_next_lines(self, lines):
+        for i in range(lines):
+            if not self.do_next_line():
                 print("End of Program")
+                break
+    
+    def do_next_line(self):
+        if len(self.statements) <= self.ip:
+            # advance_stmt() will leave self.ip in this state
+            # only if End of Program is reached
+            return False
+        
+        stmt = self.statements[self.ip]
+        cur_line_number = stmt.line_number
+        while True:
+            self.execute_stmt()
+            self.advance_stmt()
+            if len(self.statements) <= self.ip:
+                # advance_stmt() will leave self.ip in this state
+                # only if End of Program is reached
+                return True
             else:
-                # out of a block
-                self = self.goback.pop()
+                stmt = self.statements[self.ip]
+                if stmt.line_number != cur_line_number:
+                    return True
+    
+    
+    def advance_stmt(self):
+        if self.ip >= len(self.statements):
+            if not self.goback:
+                None
+            else:
+                # change of control flow
+                scope, ip, statements = self.goback.pop()
+                self.scope = scope.deepcopy(-1)
+                self.ip = ip
+                self.statements = statements
+                
                 stmt = self.statements[self.ip]
                 if (stmt.tag == "for"):
-                    # goto stmt.fin, then evaluate condition
+                    # finished iteration of [initializer] or body + [step]
+                    # evaluate condition and set ip and scope appropriately
                     
                     # difference with gdb:
                     # gdb will evaluate condition on the following "next" command,
@@ -32,22 +64,17 @@ class State:
                     # our simulator will terminate one line earlier than gdb.
                     
                     forblk = stmt
-                    self.do(forblk.fin)
-                    if bool(forblk.condition.eval()):
-                        self.goback.append(self) # deepcopy?
+                    if bool(self.evaluate(forblk.condition)):
+                        self.goback.append(
+                            (self.scope.deepcopy(1), self.ip, list(self.statements))
+                        )
                         self.ip = 0
-                        self.statements = forblk.body
+                        self.statements = forblk.body + [forblk.step]
                     else:
                         self.ip += 1
-        
-        else:
-            stmt = self.statements[self.ip]
-            self.do(stmt)
-            self.ip += 1
-        
-        return self
+                # TODO: Other cases
     
-    def do(self, stmt, aux = None):
+    def execute_stmt(self, stmt, aux = None):
         if (stmt.tag == "function"):
             function = stmt
             self.ftable.define(
@@ -73,5 +100,3 @@ class State:
         elif (stmt.tag == "for"):
             forblk = stmt
             forblk.initial
-        
-        
